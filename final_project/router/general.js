29d1ac65aaab3a6a641325e68293d2ac19769717
Angel Jonathan Carrillo Flores <leviskrip@gmail.com>
2023-12-14 23:37:53 -0600

    Default, byISBN, byAuthor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,58 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //let libros = JSON.stringify(this.books);
+
+  return res.status(200).json(books);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+  let isbn = req.params["isbn"];
+  // if (isNaN(isbn)){
+  //   return res.status(300).json({message: "ISBN not valid."})
+  // }
+  //let toSend = books.map();
+  //let arr = Object.values(books);
+  let found = books[isbn];
+  if (! found){
+    return res.status(300).json({error: `ISBN "${isbn}" not found or not valid.`})
+  }
+  // console.log(`ISBN: ${isbn}`);
+  // console.log(`Tipo books: ${typeof books}`);
+  
+  //let found = arr.filter((book)=> book.isbn === isbn);
+  //console.log(`Encontre ${found} de tipo ${typeof found}`);
+
+  return res.status(200).json(books[isbn]);
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params["author"];
+  
+  let arrBooks = Object.values(books);
+  // console.log(books);
+  // console.log(arrBooks);
+  let arrISBN = Object.keys(books);
+  let iter = 0;
+  let found = {};
+  for(let book in arrBooks){
+    
+    console.log(typeof arrBooks[book]);
+    let isbn = arrISBN[iter];
+    if(arrBooks[book].author === author){
+      
+      found[isbn] = arrBooks[book];
+    }
+    iter+=1;
+  }
+  
+  return res.status(300).json(found);
 });
 
 // Get all books based on title
